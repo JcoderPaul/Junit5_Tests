0c3116e586a050fa6b14d6f1e27d7efa77c73d89
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-06-30 12:44:48 +0300

    Add CreateUserValidator.java in Junit5_Practice

---
+++ Junit5_Practice/src/main/java/com/oldboy/Validator/CreateUserValidator.java
@@ -0,0 +1,34 @@
+package com.oldboy.Validator;
+
+import com.oldboy.DTO.CreateUserDto;
+import com.oldboy.Entity.Enum.Gender;
+import com.oldboy.Entity.Enum.Role;
+import com.oldboy.Util.LocalDateFormatter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class CreateUserValidator implements Validator<CreateUserDto> {
+
+    private static final CreateUserValidator INSTANCE = new CreateUserValidator();
+
+    public static CreateUserValidator getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public ValidationResult validate(CreateUserDto object) {
+        ValidationResult validationResult = new ValidationResult();
+        if (!LocalDateFormatter.isValid(object.getBirthday())) {
+            validationResult.add(Error.of("invalid.birthday", "Birthday is invalid"));
+        }
+        if (Gender.find(object.getGender()).isEmpty()) {
+            validationResult.add(Error.of("invalid.gender", "Gender is invalid"));
+        }
+        if (Role.find(object.getRole()).isEmpty()) {
+            validationResult.add(Error.of("invalid.role", "Role is invalid"));
+        }
+        return validationResult;
+    }
+}
