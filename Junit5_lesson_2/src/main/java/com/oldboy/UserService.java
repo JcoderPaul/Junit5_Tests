79b0cc2cd8eff59262e19279a794c3903844a2eb
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-06-30 11:44:44 +0300

    Add UserService.java in Junit5_lesson_2

---
+++ Junit5_lesson_2/src/main/java/com/oldboy/UserService.java
@@ -0,0 +1,36 @@
+package com.oldboy;
+/*
+Создаем данный класс и пока у него только один метод *.getAll(),
+который возвращает пустую коллекцию (база данных пуста, ее даже
+нет еще), но метод существовать может и даже что-то возвращать.
+
+Поскольку мы должны вернуть список неких сущностей (объектов), нам
+их надо создать это класс User в папке DTO.
+*/
+import com.oldboy.DTO.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+    /*
+    Естественно в реальном проекте мы бы организовали
+    связь с базой и проводили все манипуляции с ней.
+    */
+    private final List<User> testUsersBase = new ArrayList<>();
+    public List<User> getAll() {
+        return testUsersBase;
+    }
+
+    public boolean addUser(User user) {
+          return testUsersBase.add(user);
+    }
+
+    public Optional<User> login(String username, String password) {
+        return testUsersBase.stream().
+                filter(userFromBase -> userFromBase.getUsername().equals(username)).
+                filter(userFromBase -> userFromBase.getPassword().equals(password)).
+                findFirst();
+    }
+}
